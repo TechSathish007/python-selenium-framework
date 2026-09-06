a0c7adfb3e75ac10637035c3d91ec441ed39df43
Sathish R <sathishrsk120@gmail.com>
2026-09-06 19:37:37 +0530

    added support for complex web elements like alerts and dropdowns

--- pages.py
+++ pages.py
@@ -1,31 +1,23 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import Select # NEW TOOL FOR DROPDOWNS
 
 class TextBoxPage:
     def __init__(self, driver):
         self.driver = driver
-        # THIS IS THE BRAIN: Wait up to 10 seconds for things to happen
         self.wait = WebDriverWait(driver, 10) 
-
     def load(self):
         self.driver.get("https://demoqa.com/text-box")
-
     def fill_form(self, name, email):
-        # SMART WAIT: Wait exactly until the box is visible before typing
         name_box = self.wait.until(EC.visibility_of_element_located((By.ID, "userName")))
         name_box.send_keys(name)
-        
         self.driver.find_element(By.ID, "userEmail").send_keys(email)
-
     def click_submit(self):
-        # SMART WAIT: Wait exactly until the button is clickable
         submit_btn = self.wait.until(EC.element_to_be_clickable((By.ID, "submit")))
-        self.driver.execute_script("arguments[0].scrollIntoView();", submit_btn) # Scroll down to it
-        self.driver.execute_script("arguments[0].click();", submit_btn) # Click it safely
-
+        self.driver.execute_script("arguments[0].scrollIntoView();", submit_btn)
+        self.driver.execute_script("arguments[0].click();", submit_btn)
     def get_output_name(self):
-        # SMART WAIT: Wait for the result to pop up on screen
         output = self.wait.until(EC.visibility_of_element_located((By.ID, "name")))
         return output.text
 
@@ -33,15 +25,53 @@ class RadioButtonPage:
     def __init__(self, driver):
         self.driver = driver
         self.wait = WebDriverWait(driver, 10)
-
     def load(self):
         self.driver.get("https://demoqa.com/radio-button")
-
     def click_yes(self):
-        # SMART WAIT: Wait for the Yes button to be clickable
         yes_label = self.wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "label[for='yesRadio']")))
         yes_label.click()
-
     def get_success_message(self):
         message = self.wait.until(EC.visibility_of_element_located((By.CLASS_NAME, "text-success")))
         return message.text
+
+# --- THE NEW ENTERPRISE FEATURES ---
+
+class AlertsPage:
+    def __init__(self, driver):
+        self.driver = driver
+        self.wait = WebDriverWait(driver, 10)
+        
+    def load(self):
+        self.driver.get("https://demoqa.com/alerts")
+        
+    def click_alert_button(self):
+        btn = self.wait.until(EC.element_to_be_clickable((By.ID, "alertButton")))
+        self.driver.execute_script("arguments[0].click();", btn)
+        
+    def accept_alert(self):
+        self.wait.until(EC.alert_is_present()) # 1. Wait for popup
+        alert = self.driver.switch_to.alert    # 2. Switch robot's brain to the popup
+        text = alert.text
+        alert.accept()                         # 3. Click the "OK" button
+        return text
+
+class DropdownPage:
+    def __init__(self, driver):
+        self.driver = driver
+        self.wait = WebDriverWait(driver, 10)
+        
+    def load(self):
+        self.driver.get("https://demoqa.com/select-menu")
+        
+    def select_color(self, color_name):
+        # 1. Find the dropdown menu
+        element = self.wait.until(EC.visibility_of_element_located((By.ID, "oldSelectMenu")))
+        # 2. Convert it into a special Selenium Select object
+        dropdown = Select(element) 
+        # 3. Tell it to pick an option by its text
+        dropdown.select_by_visible_text(color_name)
+        
+    def get_selected_color(self):
+        element = self.driver.find_element(By.ID, "oldSelectMenu")
+        dropdown = Select(element)
+        return dropdown.first_selected_option.text
--- test_pom.py
+++ test_pom.py
@@ -1,6 +1,6 @@
 import pytest
 import csv
-from pages import TextBoxPage, RadioButtonPage
+from pages import TextBoxPage, RadioButtonPage, AlertsPage, DropdownPage
 
 # --- 1. OUR SPREADSHEET READER ---
 def get_csv_data():
@@ -8,27 +8,48 @@ def get_csv_data():
     with open("test_data.csv", "r") as file:
         reader = csv.reader(file)
         next(reader)  # Skips the header row (name,email)
-        
+
         for row in reader:
             data_list.append(tuple(row))
-            
+
     return data_list
 
 # --- 2. WE PLUG THE READER DIRECTLY INTO PYTEST ---
-# Pytest will now open the CSV and loop this test for every row it finds!
 @pytest.mark.parametrize("name, email", get_csv_data())
 def test_text_box_clean(driver, name, email):
     text_box_page = TextBoxPage(driver)
     text_box_page.load()
     
-    text_box_page.fill_form(name, email) 
+    text_box_page.fill_form(name, email)
     text_box_page.click_submit()
     
-    assert name in text_box_page.get_output_name() 
+    assert name in text_box_page.get_output_name()
 
-# (The radio button test stays exactly the same)
 def test_radio_button_clean(driver):
     radio_page = RadioButtonPage(driver)
     radio_page.load()
     radio_page.click_yes()
     assert "Yes" in radio_page.get_success_message()
+
+# --- 3. COMPLEX WEB ELEMENTS ---
+def test_alert_popup(driver):
+    alert_page = AlertsPage(driver)
+    alert_page.load()
+    
+    # Click the button to trigger the alert
+    alert_page.click_alert_button()
+    
+    # Accept it and check what it said
+    alert_text = alert_page.accept_alert()
+    assert alert_text == "You clicked a button"
+
+def test_dropdown_menu(driver):
+    dropdown_page = DropdownPage(driver)
+    dropdown_page.load()
+    
+    # Select "Yellow" from the dropdown list
+    dropdown_page.select_color("Yellow")
+    
+    # Verify that Yellow is actually the active selection now
+    selected = dropdown_page.get_selected_color()
+    assert selected == "Yellow"
