2979583b2b38781905b6662ea41e96cc658c9802
Sathish R <sathishrsk120@gmail.com>
2026-09-06 19:24:27 +0530

    added cross-browser testing for chrome, firefox, and edge

--- conftest.py
+++ conftest.py
@@ -1,19 +1,36 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
+from selenium.webdriver.edge.options import Options as EdgeOptions
+
+# 1. We create a custom terminal command called "--browser"
+def pytest_addoption(parser):
+    parser.addoption("--browser", action="store", default="chrome")
 
 @pytest.fixture
-def driver():
-    # 1. Set up Chrome Options
-    chrome_options = Options()
-    chrome_options.add_argument("--headless") # This makes it invisible!
-    chrome_options.add_argument("--window-size=1920,1080") # Gives it a virtual screen size
+def driver(request):
+    # 2. We ask Pytest which browser the user typed in the terminal
+    browser_name = request.config.getoption("--browser")
     
-    # 2. Launch Chrome invisibly
-    driver = webdriver.Chrome(options=chrome_options)
-    
-    # 3. Hand the invisible browser to the test
+    # 3. We use an IF statement to launch the correct one!
+    if browser_name == "chrome":
+        options = ChromeOptions()
+        options.add_argument("--headless")
+        driver = webdriver.Chrome(options=options)
+        
+    elif browser_name == "firefox":
+        options = FirefoxOptions()
+        options.add_argument("--headless")
+        driver = webdriver.Firefox(options=options)
+        
+    elif browser_name == "edge":
+        options = EdgeOptions()
+        options.add_argument("--headless")
+        driver = webdriver.Edge(options=options)
+        
+    # 4. Hand the browser to the test
     yield driver
     
-    # 4. Quit when finished
+    # 5. Quit when finished
     driver.quit()
